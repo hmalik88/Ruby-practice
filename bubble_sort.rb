a9b04446cc49e7bebc837a3abed7a5a2f9fd67fb
hmalik88 <hbmalik88@gmail.com>
2020-05-05 00:41:51 -0400

    added benchmarking

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,23 @@
+require 'benchmark'
+
+def benchmark
+  n = 50000
+  Benchmark.bm do |benchmark|
+    benchmark.report('With flag') do
+      n.times do
+        bubble_sort([4,3,78,2,0,2])
+      end
+    end
+
+    benchmark.report('Without flag') do
+      n.times do
+        bubble_sort_no_flag([4,3,78,2,0,2])
+      end
+    end
+  end
+end
+
+
 def bubble_sort(arr)
   swap = false
   for j in 0...arr.length
@@ -14,4 +34,17 @@ def bubble_sort(arr)
   arr
 end
 
-p bubble_sort([4,3,78,2,0,2])
+def bubble_sort_no_flag(arr)
+  for j in 0...arr.length
+    for k in 0...arr.length-j-1
+      if arr[k+1] < arr[k]
+        tmp = arr[k]
+        arr[k] = arr[k+1]
+        arr[k+1] = tmp
+      end
+    end
+  end
+  arr
+end
+
+p benchmark()
